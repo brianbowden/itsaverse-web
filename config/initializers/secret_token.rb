ce3b7eb0d1c9b6e8b60d55d4efba4db8f4ad7637
Brian Bowden <brian@bitstomp.com>
2014-03-14 21:53:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItsaverseWeb::Application.config.secret_key_base = 'ce18882f98042ee5606fedbf33e547233b9d09adb23c91c0915c64998d5d591b71f1e98f0af540336eb7eb07b9daa6c01bd470883d5a5a61ffb9e9fc1ba6bd2f'
