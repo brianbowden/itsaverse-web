c5fae475733f3e597ea9fbd92973ee21ee066b97
Brian Bowden <brian@bitstomp.com>
2014-07-19 14:40:43 -0400

    Yet another unicorn config fix

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/dev/apps/Itsaverse-Web/current
+APP_ROOT=/home/dev/apps/itsaverse-web/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=dev
